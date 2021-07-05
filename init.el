62b1e8803d175dac964ff5d2db6ab3f9402f5b30
Billy Clack <bclack@nvidia.com>
2021-07-05 10:32:16 -0700

    Updating with zenburn theme and customizations

--- init.el
+++ init.el
@@ -3,6 +3,10 @@
 (package-initialize)
 (package-refresh-contents)
 
+;;
+;; Evil
+;;
+
 ;; Download Evil
 (unless (package-installed-p 'evil)
   (package-install 'evil))
@@ -15,10 +19,42 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
+ '(package-selected-packages '(zenburn-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;
+;; Theme
+;;
+
+;; Zenburn - https://github.com/bbatsov/zenburn-emacs
+(unless (package-installed-p 'zenburn-theme)
+  (package-install 'zenburn-theme))
+
+;; Customize some colors
+(setq zenburn-override-colors-alist
+      '(("zenburn-bg+05" . "#282828")
+        ("zenburn-bg+1"  . "#2F2F2F")
+        ("zenburn-bg+2"  . "#3F3F3F")
+        ("zenburn-bg+3"  . "#4F4F4F")))
+
+;; use variable-pitch fonts for some headings and titles
+(setq zenburn-use-variable-pitch t)
+
+;; scale headings in org-mode
+(setq zenburn-scale-org-headlines t)
+
+;; scale headings in outline-mode
+(setq zenburn-scale-outline-headlines t)
+
+;; Change the "todo" to a brigher color of red
+;; DONT KNOW HOW TO DO THIS YET
+;;(custom-theme-set-faces
+;; 'zenburn
+;; '(org-todo ((t (:weight bold :foreground ,zenburn-blue :weight bold)))))
+
+(load-theme 'zenburn t)
